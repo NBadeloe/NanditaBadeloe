0bbf2f2b59c2c5adc2efc937ebb9bae708632cce
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2025-09-03 13:19:26 +0200

    intial commit, working on responsive navbar

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,75 @@
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/">Design</a></li>
+    <h1>Nandita Badeloe</h1>
+    <li><a href="/">Projects</a></li>
+    <li><a href="/">Contact</a></li>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    background-color: aqua;
+    color: #f9f9f9;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 4em;
+    width: 90%;
+    margin: auto;
+    border-radius: 2em;
+    border: 2px #f9f9f9 solid;
+  }
+  nav > ul {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  nav > ul,
+  li {
+    list-style: none;
+    padding: 1em;
+    margin: 1em 4em;
+  }
+
+  nav > ul a{
+    color: #f9f9f9;
+    text-decoration: none;
+  }
+  @media screen and (min-width: 390px) and (max-width: 950px) {
+    nav {
+     background-color:pink;
+      height: 3.5em;
+      flex-direction: column;
+    }
+      nav > ul,
+  li {
+    list-style: none;
+    padding: 0.5em;
+    margin: 1.5em 2em;
+    align-items: center;
+  }
+  ul > h1{
+    margin: 0 1em;
+  }
+  }
+    @media screen and (min-width: 0px) and (max-width: 650px) {
+    nav {
+     background-color:red;
+      height: 3.5em;
+      flex-direction: column;
+    }
+      nav > ul,
+  li {
+    list-style: none;
+    padding: 0.5em;
+    margin: 1.5em 2em;
+    align-items: center;
+  }
+  ul > h1{
+    margin: 0 1em;
+  }
+
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Nav from "$lib/components/Nav.svelte";
+</script>
+
+<Nav/>
+
+<style>
+    :global(body) {
+        background-color: gainsboro;
+    }
+</style>
